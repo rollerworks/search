c31ff499ef80543f8f7cb9ea298ef60fa7f079d7
Sebastiaan Stok <s.stok@rollerscapes.net>
2012-03-25 10:48:31 +0200

    [Record] DomainAwareSQLRecordInterface, restrict domain-specific configuration by an interface

---
+++ Record/SQL/DomainAwareSQLRecordInterface.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * This file is part of the RollerworksRecordFilterBundle.
+ *
+ * (c) Rollerscapes
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @link    http://projects.rollerscapes.net/RollerFramework
+ * @license http://www.opensource.org/licenses/lgpl-license.php LGPL
+ */
+
+namespace Rollerworks\RecordFilterBundle\Record\SQL;
+
+/**
+ * DomainAwareSQLRecord interface should be implemented by SQL record classes that are domain-aware.
+ *
+ * An class is domain-aware when the configuration only applies to one class (domain).
+ *
+ * @author Sebastiaan Stok <s.stok@rollerscapes.net>
+ */
+interface DomainAwareSQLRecordInterface extends SQLRecordInterface
+{
+    /**
+     * Returns the class name from which the configuration was read.
+     *
+     * @return string
+     */
+    public function getBaseClassName();
+}
