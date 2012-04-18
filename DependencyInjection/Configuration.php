53a3b2329dbb465897f82effc0097c196ea7c2fa
Sebastiaan Stok <s.stok@rollerscapes.net>
2012-04-18 11:39:04 +0200

    Added missing file.

---
+++ DependencyInjection/Configuration.php
@@ -0,0 +1,44 @@
+<?php
+
+/**
+ * This file is part of the RollerworksRecordFilterBundle package.
+ *
+ * (c) Sebastiaan Stok <s.stok@rollerscapes.net>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Rollerworks\RecordFilterBundle\DependencyInjection;
+
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+
+/**
+ * This class contains the configuration information for the bundle
+ *
+ * This information is solely responsible for how the different configuration
+ * sections are normalized, and merged.
+ *
+ * @author Sebastiaan Stok <s.stok@rollerscapes.net>
+ */
+class Configuration implements ConfigurationInterface
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function getConfigTreeBuilder()
+    {
+        $treeBuilder = new TreeBuilder();
+
+        $treeBuilder->root('rollerworks_record_filter')
+            ->children()
+                ->scalarNode('metadata_cache')->defaultValue('%kernel.cache_dir%/recordfilter_metedata')->end()
+            ->end()
+        ->end();
+
+        return $treeBuilder;
+    }
+}
+
