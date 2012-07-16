646988ae3438f99eb69cbe25a9d9c29af55f6339
Sebastiaan Stok <s.stok@rollerscapes.net>
2012-07-16 14:16:29 +0200

    fixed compiler passes were no registered in the bundle

--- RollerworksRecordFilterBundle.php
+++ RollerworksRecordFilterBundle.php
@@ -11,9 +11,19 @@
 
 namespace Rollerworks\Bundle\RecordFilterBundle;
 
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+use Rollerworks\Bundle\RecordFilterBundle\DependencyInjection\Compiler\AddFilterTypesPass;
+use Rollerworks\Bundle\RecordFilterBundle\DependencyInjection\Compiler\FormatterModifiersPass;
+
 /**
  * RollerworksRecordFilterBundle.
  */
 class RollerworksRecordFilterBundle extends \Symfony\Component\HttpKernel\Bundle\Bundle
 {
+    public function build(ContainerBuilder $container)
+    {
+        $container->addCompilerPass(new AddFilterTypesPass());
+        $container->addCompilerPass(new FormatterModifiersPass());
+    }
 }
