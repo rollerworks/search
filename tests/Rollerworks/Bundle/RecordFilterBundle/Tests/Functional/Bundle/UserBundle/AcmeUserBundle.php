28b351dbe431102ee251c037535920bd9bf14f3b
Sebastiaan Stok <s.stok@rollerscapes.net>
2013-08-10 11:26:01 +0200

    removed old Files

--- tests/Rollerworks/Bundle/RecordFilterBundle/Tests/Functional/Bundle/UserBundle/AcmeUserBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Rollerworks\Bundle\RecordFilterBundle\Tests\Functional\Bundle\UserBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class AcmeUserBundle extends Bundle
-{
-}
