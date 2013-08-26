3bc7bba16d289a4eb34b1f482691a4c643e57614
Sebastiaan Stok <s.stok@rollerscapes.net>
2013-08-26 16:40:34 +0200

    added Formatter/DuplicateRemover

---
+++ spec/Rollerworks/Component/Search/Formatter/DuplicateRemoverSpec.php
@@ -0,0 +1,317 @@
+<?php
+
+/*
+ * This file is part of the Rollerworks Search Component package.
+ *
+ * (c) Sebastiaan Stok <s.stok@rollerscapes.net>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace spec\Rollerworks\Component\Search\Formatter;
+
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+use Rollerworks\Component\Search\FieldConfigInterface;
+use Rollerworks\Component\Search\FieldSet;
+use Rollerworks\Component\Search\SearchConditionInterface;
+use Rollerworks\Component\Search\Value\Compare;
+use Rollerworks\Component\Search\Value\PatternMatch;
+use Rollerworks\Component\Search\Value\Range;
+use Rollerworks\Component\Search\Value\SingleValue;
+use Rollerworks\Component\Search\ValueComparisonInterface;
+use Rollerworks\Component\Search\ValuesBag;
+use Rollerworks\Component\Search\ValuesGroup;
+
+class DuplicateRemoverSpec extends ObjectBehavior
+{
+    function it_is_initializable()
+    {
+        $this->shouldHaveType('Rollerworks\Component\Search\Formatter\DuplicateRemover');
+        $this->shouldImplement('Rollerworks\Component\Search\FormatterInterface');
+    }
+
+    function it_removes_all_duplicated_singleValues(SearchConditionInterface $condition, FieldSet $fieldSet, FieldConfigInterface $dateField, ValuesBag $valuesBag, ValueComparisonInterface $comparison)
+    {
+        $comparison->isEqual(Argument::any(), Argument::any(), Argument::type('array'))->will(function ($args) {
+            return $args[0] === $args[1];
+        });
+
+        $currentValues = array(
+            new SingleValue('2013-08-25 00:00:00'),
+            new SingleValue('2013-08-30 00:00:00'),
+            new SingleValue('2013-08-25 00:00:00'),
+            new SingleValue('2013-08-10 00:00:00'),
+            new SingleValue('2013-08-25 00:00:00'),
+            new SingleValue('2013-08-15 00:00:00'),
+            new SingleValue('2013-08-15 00:00:00'),
+        );
+
+        $valuesBag->getSingleValues()->willReturn($currentValues);
+        $valuesBag->hasSingleValues()->willReturn(true);
+        $valuesBag->hasExcludedValues()->willReturn(false);
+        $valuesBag->hasRanges()->willReturn(false);
+        $valuesBag->hasExcludedRanges()->willReturn(false);
+        $valuesBag->hasComparisons()->willReturn(false);
+        $valuesBag->hasPatternMatchers()->willReturn(false);
+
+        $valuesBag->removeSingleValue(2)->shouldBeCalledTimes(2);
+        $valuesBag->removeSingleValue(4)->shouldBeCalledTimes(2);
+        $valuesBag->removeSingleValue(6)->shouldBeCalledTimes(2);
+
+        $dateField->hasOption('constraints')->willReturn(false);
+        $dateField->getOptions()->willReturn(array());
+        $dateField->getValueComparison()->willReturn($comparison);
+
+        $fieldSet->get('date')->willReturn($dateField);
+        $fieldSet->has('date')->willReturn(true);
+
+        $valuesGroup = new ValuesGroup();
+        $valuesGroup->addField('date', $valuesBag->getWrappedObject());
+
+        $valuesGroup2 = new ValuesGroup();
+        $valuesGroup2->addField('date', $valuesBag->getWrappedObject());
+        $valuesGroup->addGroup($valuesGroup2);
+
+        $condition->getValuesGroup()->willReturn($valuesGroup);
+        $condition->getFieldSet()->willReturn($fieldSet);
+
+        $this->format($condition);
+    }
+
+    function it_removes_all_duplicated_excludedValues(SearchConditionInterface $condition, FieldSet $fieldSet, FieldConfigInterface $dateField, ValuesBag $valuesBag, ValueComparisonInterface $comparison)
+    {
+        $comparison->isEqual(Argument::any(), Argument::any(), Argument::type('array'))->will(function ($args) {
+            return $args[0] === $args[1];
+        });
+
+        $currentValues = array(
+            new SingleValue('2013-08-25 00:00:00'),
+            new SingleValue('2013-08-30 00:00:00'),
+            new SingleValue('2013-08-25 00:00:00'),
+            new SingleValue('2013-08-10 00:00:00'),
+            new SingleValue('2013-08-25 00:00:00')
+        );
+
+        $valuesBag->getExcludedValues()->willReturn($currentValues);
+        $valuesBag->hasSingleValues()->willReturn(false);
+        $valuesBag->hasExcludedValues()->willReturn(true);
+        $valuesBag->hasRanges()->willReturn(false);
+        $valuesBag->hasExcludedRanges()->willReturn(false);
+        $valuesBag->hasComparisons()->willReturn(false);
+        $valuesBag->hasPatternMatchers()->willReturn(false);
+
+        $valuesBag->removeExcludedValue(2)->shouldBeCalledTimes(2);
+        $valuesBag->removeExcludedValue(4)->shouldBeCalledTimes(2);
+
+        $dateField->hasOption('constraints')->willReturn(false);
+        $dateField->getOptions()->willReturn(array());
+        $dateField->getValueComparison()->willReturn($comparison);
+
+        $fieldSet->get('date')->willReturn($dateField);
+        $fieldSet->has('date')->willReturn(true);
+
+        $valuesGroup = new ValuesGroup();
+        $valuesGroup->addField('date', $valuesBag->getWrappedObject());
+
+        $valuesGroup2 = new ValuesGroup();
+        $valuesGroup2->addField('date', $valuesBag->getWrappedObject());
+        $valuesGroup->addGroup($valuesGroup2);
+
+        $condition->getValuesGroup()->willReturn($valuesGroup);
+        $condition->getFieldSet()->willReturn($fieldSet);
+
+        $this->format($condition);
+    }
+
+    function it_removes_all_duplicated_ranges(SearchConditionInterface $condition, FieldSet $fieldSet, FieldConfigInterface $dateField, ValuesBag $valuesBag, ValueComparisonInterface $comparison)
+    {
+        $comparison->isEqual(Argument::any(), Argument::any(), Argument::type('array'))->will(function ($args) {
+            return $args[0] === $args[1];
+        });
+
+        $currentValues = array(
+            new Range('2013-08-25 00:00:00', '2013-08-30 00:00:00'),
+            new Range('2013-09-25 00:00:00', '2013-09-30 00:00:00'),
+            new Range('2013-08-25 00:00:00', '2013-08-30 00:00:00'),
+            new Range('2013-10-25 00:00:00', '2013-10-30 00:00:00'),
+            new Range('2013-08-25 00:00:00', '2013-08-30 00:00:00'),
+            new Range('2013-08-25 00:00:00', '2013-08-30 00:00:00', false), // duplicated but exclusive
+            new Range('2013-08-25 00:00:00', '2013-08-30 00:00:00', true, false), // duplicated but exclusive
+        );
+
+        $valuesBag->getRanges()->willReturn($currentValues);
+        $valuesBag->hasSingleValues()->willReturn(false);
+        $valuesBag->hasExcludedValues()->willReturn(false);
+        $valuesBag->hasRanges()->willReturn(true);
+        $valuesBag->hasExcludedRanges()->willReturn(false);
+        $valuesBag->hasComparisons()->willReturn(false);
+        $valuesBag->hasPatternMatchers()->willReturn(false);
+
+        $valuesBag->removeRange(2)->shouldBeCalledTimes(2);
+        $valuesBag->removeRange(4)->shouldBeCalledTimes(2);
+
+        $dateField->hasOption('constraints')->willReturn(false);
+        $dateField->getOptions()->willReturn(array());
+        $dateField->getValueComparison()->willReturn($comparison);
+
+        $fieldSet->get('date')->willReturn($dateField);
+        $fieldSet->has('date')->willReturn(true);
+
+        $valuesGroup = new ValuesGroup();
+        $valuesGroup->addField('date', $valuesBag->getWrappedObject());
+
+        $valuesGroup2 = new ValuesGroup();
+        $valuesGroup2->addField('date', $valuesBag->getWrappedObject());
+        $valuesGroup->addGroup($valuesGroup2);
+
+        $condition->getValuesGroup()->willReturn($valuesGroup);
+        $condition->getFieldSet()->willReturn($fieldSet);
+
+        $this->format($condition);
+    }
+
+    function it_removes_all_duplicated_excludedRanges(SearchConditionInterface $condition, FieldSet $fieldSet, FieldConfigInterface $dateField, ValuesBag $valuesBag, ValueComparisonInterface $comparison)
+    {
+        $comparison->isEqual(Argument::any(), Argument::any(), Argument::type('array'))->will(function ($args) {
+            return $args[0] === $args[1];
+        });
+
+        $currentValues = array(
+            new Range('2013-08-25 00:00:00', '2013-08-30 00:00:00'),
+            new Range('2013-09-25 00:00:00', '2013-09-30 00:00:00'),
+            new Range('2013-08-25 00:00:00', '2013-08-30 00:00:00'),
+            new Range('2013-10-25 00:00:00', '2013-10-30 00:00:00'),
+            new Range('2013-08-25 00:00:00', '2013-08-30 00:00:00'),
+            new Range('2013-08-25 00:00:00', '2013-08-30 00:00:00', false), // duplicated but exclusive
+            new Range('2013-08-25 00:00:00', '2013-08-30 00:00:00', true, false), // duplicated but exclusive
+        );
+
+        $valuesBag->getExcludedRanges()->willReturn($currentValues);
+        $valuesBag->hasSingleValues()->willReturn(false);
+        $valuesBag->hasExcludedValues()->willReturn(false);
+        $valuesBag->hasRanges()->willReturn(false);
+        $valuesBag->hasExcludedRanges()->willReturn(true);
+        $valuesBag->hasComparisons()->willReturn(false);
+        $valuesBag->hasPatternMatchers()->willReturn(false);
+
+        $valuesBag->removeExcludedRange(2)->shouldBeCalledTimes(2);
+        $valuesBag->removeExcludedRange(4)->shouldBeCalledTimes(2);
+
+        $dateField->hasOption('constraints')->willReturn(false);
+        $dateField->getOptions()->willReturn(array());
+        $dateField->getValueComparison()->willReturn($comparison);
+
+        $fieldSet->get('date')->willReturn($dateField);
+        $fieldSet->has('date')->willReturn(true);
+
+        $valuesGroup = new ValuesGroup();
+        $valuesGroup->addField('date', $valuesBag->getWrappedObject());
+
+        $valuesGroup2 = new ValuesGroup();
+        $valuesGroup2->addField('date', $valuesBag->getWrappedObject());
+        $valuesGroup->addGroup($valuesGroup2);
+
+        $condition->getValuesGroup()->willReturn($valuesGroup);
+        $condition->getFieldSet()->willReturn($fieldSet);
+
+        $this->format($condition);
+    }
+
+    function it_removes_all_duplicated_comparison(SearchConditionInterface $condition, FieldSet $fieldSet, FieldConfigInterface $dateField, ValuesBag $valuesBag, ValueComparisonInterface $comparison)
+    {
+        $comparison->isEqual(Argument::any(), Argument::any(), Argument::type('array'))->will(function ($args) {
+            return $args[0] === $args[1];
+        });
+
+        $currentValues = array(
+            new Compare('2013-08-25 00:00:00', '>'),
+            new Compare('2013-08-25 00:00:00', '<'),
+            new Compare('2013-08-25 00:00:00', '>'),
+            new Compare('2013-08-25 10:00:00', '>'),
+            new Compare('2013-08-25 00:00:00', '>'),
+            new Compare('2013-08-25 00:00:00', '>='),
+            new Compare('2013-08-25 00:00:00', '>='),
+        );
+
+        $valuesBag->getComparisons()->willReturn($currentValues);
+        $valuesBag->hasSingleValues()->willReturn(false);
+        $valuesBag->hasExcludedValues()->willReturn(false);
+        $valuesBag->hasRanges()->willReturn(false);
+        $valuesBag->hasExcludedRanges()->willReturn(false);
+        $valuesBag->hasComparisons()->willReturn(true);
+        $valuesBag->hasPatternMatchers()->willReturn(false);
+
+        $valuesBag->removeComparison(2)->shouldBeCalledTimes(2);
+        $valuesBag->removeComparison(4)->shouldBeCalledTimes(2);
+        $valuesBag->removeComparison(6)->shouldBeCalledTimes(2);
+
+        $dateField->hasOption('constraints')->willReturn(false);
+        $dateField->getOptions()->willReturn(array());
+        $dateField->getValueComparison()->willReturn($comparison);
+
+        $fieldSet->get('date')->willReturn($dateField);
+        $fieldSet->has('date')->willReturn(true);
+
+        $valuesGroup = new ValuesGroup();
+        $valuesGroup->addField('date', $valuesBag->getWrappedObject());
+
+        $valuesGroup2 = new ValuesGroup();
+        $valuesGroup2->addField('date', $valuesBag->getWrappedObject());
+        $valuesGroup->addGroup($valuesGroup2);
+
+        $condition->getValuesGroup()->willReturn($valuesGroup);
+        $condition->getFieldSet()->willReturn($fieldSet);
+
+        $this->format($condition);
+    }
+
+    function it_removes_all_duplicated_matchers(SearchConditionInterface $condition, FieldSet $fieldSet, FieldConfigInterface $dateField, ValuesBag $valuesBag, ValueComparisonInterface $comparison)
+    {
+        $comparison->isEqual(Argument::any(), Argument::any(), Argument::type('array'))->will(function ($args) {
+            return $args[0] === $args[1];
+        });
+
+        $currentValues = array(
+            new PatternMatch('foo', PatternMatch::PATTERN_CONTAINS),
+            new PatternMatch('bar', PatternMatch::PATTERN_CONTAINS),
+            new PatternMatch('foo', PatternMatch::PATTERN_CONTAINS),
+            new PatternMatch('foo', PatternMatch::PATTERN_ENDS_WITH),
+            new PatternMatch('foo', PatternMatch::PATTERN_ENDS_WITH),
+            new PatternMatch('bla', PatternMatch::PATTERN_CONTAINS),
+            new PatternMatch('bar', PatternMatch::PATTERN_CONTAINS),
+        );
+
+        $valuesBag->getPatternMatchers()->willReturn($currentValues);
+        $valuesBag->hasSingleValues()->willReturn(false);
+        $valuesBag->hasExcludedValues()->willReturn(false);
+        $valuesBag->hasRanges()->willReturn(false);
+        $valuesBag->hasExcludedRanges()->willReturn(false);
+        $valuesBag->hasComparisons()->willReturn(false);
+        $valuesBag->hasPatternMatchers()->willReturn(true);
+
+        $valuesBag->removePatternMatch(2)->shouldBeCalledTimes(2);
+        $valuesBag->removePatternMatch(4)->shouldBeCalledTimes(2);
+        $valuesBag->removePatternMatch(6)->shouldBeCalledTimes(2);
+
+        $dateField->hasOption('constraints')->willReturn(false);
+        $dateField->getOptions()->willReturn(array());
+        $dateField->getValueComparison()->willReturn($comparison);
+
+        $fieldSet->get('date')->willReturn($dateField);
+        $fieldSet->has('date')->willReturn(true);
+
+        $valuesGroup = new ValuesGroup();
+        $valuesGroup->addField('date', $valuesBag->getWrappedObject());
+
+        $valuesGroup2 = new ValuesGroup();
+        $valuesGroup2->addField('date', $valuesBag->getWrappedObject());
+        $valuesGroup->addGroup($valuesGroup2);
+
+        $condition->getValuesGroup()->willReturn($valuesGroup);
+        $condition->getFieldSet()->willReturn($fieldSet);
+
+        $this->format($condition);
+    }
+}
---
+++ src/Rollerworks/Component/Search/Formatter/DuplicateRemover.php
@@ -0,0 +1,216 @@
+<?php
+
+/*
+ * This file is part of the Rollerworks Search Component package.
+ *
+ * (c) Sebastiaan Stok <s.stok@rollerscapes.net>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Rollerworks\Component\Search\Formatter;
+
+use Rollerworks\Component\Search\FieldConfigInterface;
+use Rollerworks\Component\Search\FormatterInterface;
+use Rollerworks\Component\Search\SearchConditionInterface;
+use Rollerworks\Component\Search\ValuesBag;
+
+/**
+ * Removes duplicated values.
+ *
+ * Duplicated values are only scanned at single-level, so if a subgroup
+ * has a value also present at a higher level its not removed.
+ *
+ *  Doing so would require to keep track of all the previous-values per-type.
+ *  Which can get very complicated very fast.
+ *
+ * Values are compared using the {@see \Rollerworks\Component\Search\ValueComparisonInterface}.
+ *
+ * @author Sebastiaan Stok <s.stok@rollerscapes.net>
+ */
+class DuplicateRemover implements FormatterInterface
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function format(SearchConditionInterface $condition)
+    {
+        $fieldSet = $condition->getFieldSet();
+        $valuesGroup = $condition->getValuesGroup();
+
+        foreach ($valuesGroup->getGroups() as $group) {
+            foreach ($group->getFields() as $fieldName => $values) {
+                if (!$fieldSet->has($fieldName)) {
+                    continue;
+                }
+
+                $config = $fieldSet->get($fieldName);
+                $this->removeDuplicates($config, $values);
+            }
+        }
+
+        foreach ($valuesGroup->getFields() as $fieldName => $values) {
+            if (!$fieldSet->has($fieldName)) {
+                continue;
+            }
+
+            $config = $fieldSet->get($fieldName);
+            $this->removeDuplicates($config, $values);
+        }
+    }
+
+    private function removeDuplicates(FieldConfigInterface $config, ValuesBag $valuesBag)
+    {
+        $comparison = $config->getValueComparison();
+        $options = $config->getOptions();
+
+        if ($valuesBag->hasSingleValues()) {
+            $singleValues = $valuesBag->getSingleValues();
+
+            foreach ($singleValues as $i => $value) {
+                if (!isset($singleValues[$i])) {
+                    continue;
+                }
+
+                foreach ($singleValues as $c => $value2) {
+                    if ($i === $c) {
+                        continue;
+                    }
+
+                    if ($comparison->isEqual($value->getValue(), $value2->getValue(), $options)) {
+                        $valuesBag->removeSingleValue($c);
+                        unset($singleValues[$c]);
+                    }
+                }
+            }
+
+            unset($singleValues);
+        }
+
+        if ($valuesBag->hasExcludedValues()) {
+            $excludedValues = $valuesBag->getExcludedValues();
+
+            foreach ($excludedValues as $i => $value) {
+                if (!isset($excludedValues[$i])) {
+                    continue;
+                }
+
+                foreach ($excludedValues as $c => $value2) {
+                    if ($i === $c) {
+                        continue;
+                    }
+
+                    if ($comparison->isEqual($value->getValue(), $value2->getValue(), $options)) {
+                        $valuesBag->removeExcludedValue($c);
+                        unset($excludedValues[$c]);
+                    }
+                }
+            }
+
+            unset($excludedValues);
+        }
+
+        if ($valuesBag->hasRanges()) {
+            $ranges = $valuesBag->getRanges();
+
+            foreach ($ranges as $i => $value) {
+                if (!isset($ranges[$i])) {
+                    continue;
+                }
+
+                foreach ($ranges as $c => $value2) {
+                    if ($i === $c) {
+                        continue;
+                    }
+
+                    // Only compare when both inclusive are equal
+                    if ($value->isLowerInclusive() !== $value2->isLowerInclusive() || $value->isUpperInclusive() !== $value2->isUpperInclusive()) {
+                        continue;
+                    }
+
+                    if ($comparison->isEqual($value->getLower(), $value2->getLower(), $options) && $comparison->isEqual($value->getUpper(), $value2->getUpper(), $options)) {
+                        $valuesBag->removeRange($c);
+                        unset($ranges[$c]);
+                    }
+                }
+            }
+
+            unset($ranges);
+        }
+
+        if ($valuesBag->hasExcludedRanges()) {
+            $ranges = $valuesBag->getExcludedRanges();
+
+            foreach ($ranges as $i => $value) {
+                foreach ($ranges as $c => $value2) {
+                    if (!isset($ranges[$i])) {
+                        continue;
+                    }
+
+                    if ($i === $c) {
+                        continue;
+                    }
+
+                    // Only compare when both inclusive are equal
+                    if ($value->isLowerInclusive() !== $value2->isLowerInclusive() || $value->isUpperInclusive() !== $value2->isUpperInclusive()) {
+                        continue;
+                    }
+
+                    if ($comparison->isEqual($value->getLower(), $value2->getLower(), $options) && $comparison->isEqual($value->getUpper(), $value2->getUpper(), $options)) {
+                        $valuesBag->removeExcludedRange($c);
+                        unset($ranges[$c]);
+                    }
+                }
+            }
+
+            unset($ranges);
+        }
+
+        if ($valuesBag->hasComparisons()) {
+            $comparisons = $valuesBag->getComparisons();
+
+            foreach ($comparisons as $i => $value) {
+                if (!isset($comparisons[$i])) {
+                    continue;
+                }
+
+                foreach ($comparisons as $c => $value2) {
+                    if ($i === $c) {
+                        continue;
+                    }
+
+                    if ($value->getOperator() === $value2->getOperator() && $comparison->isEqual($value->getValue(), $value2->getValue(), $options)) {
+                        $valuesBag->removeComparison($c);
+                        unset($comparisons[$c]);
+                    }
+                }
+            }
+
+            unset($comparisons);
+        }
+
+        if ($valuesBag->hasPatternMatchers()) {
+            $matchers = $valuesBag->getPatternMatchers();
+
+            foreach ($matchers as $i => $value) {
+                if (!isset($matchers[$i])) {
+                    continue;
+                }
+
+                foreach ($matchers as $c => $value2) {
+                    if ($i === $c) {
+                        continue;
+                    }
+
+                    if ($value->getType() === $value2->getType() && $comparison->isEqual($value->getValue(), $value2->getValue(), $options)) {
+                        $valuesBag->removePatternMatch($c);
+                        unset($matchers[$c]);
+                    }
+                }
+            }
+
+            unset($matchers);
+        }
+    }
+}
