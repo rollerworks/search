e0f3b6070f4e2460d6e780d359d94225987a0c61
Sebastiaan Stok <s.stok@rollerscapes.net>
2013-10-05 16:07:50 +0200

    added Extension/Symfony/DependencyInjection
    Provides a way to lazy load types from the Symfony2 Dependency Injection container.

---
+++ src/Rollerworks/Component/Search/Extension/Symfony/DependencyInjection/DependencyInjectionExtension.php
@@ -0,0 +1,80 @@
+<?php
+
+/*
+ * This file is part of the Rollerworks Search Component package.
+ *
+ * (c) Sebastiaan Stok <s.stok@rollerscapes.net>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Rollerworks\Component\Search\Extension\Symfony\DependencyInjection;
+
+use Rollerworks\Component\Search\Exception\InvalidArgumentException;
+use Rollerworks\Component\Search\SearchExtensionInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Provides a way to lazy load types from the Symfony2 Dependency Injection container.
+ *
+ * @author Sebastiaan Stok <s.stok@rollerscapes.net>
+ */
+class DependencyInjectionExtension implements SearchExtensionInterface
+{
+    private $container;
+
+    private $typeServiceIds;
+
+    private $typeExtensionServiceIds;
+
+    public function __construct(ContainerInterface $container, array $typeServiceIds, array $typeExtensionServiceIds)
+    {
+        $this->container = $container;
+        $this->typeServiceIds = $typeServiceIds;
+        $this->typeExtensionServiceIds = $typeExtensionServiceIds;
+    }
+
+    public function getType($name)
+    {
+        if (!isset($this->typeServiceIds[$name])) {
+            throw new InvalidArgumentException(sprintf('The field type "%s" is not registered with the service container.', $name));
+        }
+
+        $type = $this->container->get($this->typeServiceIds[$name]);
+
+        if ($type->getName() !== $name) {
+            throw new InvalidArgumentException(
+                sprintf('The type name specified for the service "%s" does not match the actual name. Expected "%s", given "%s"',
+                    $this->typeServiceIds[$name],
+                    $name,
+                    $type->getName()
+                ));
+        }
+
+        return $type;
+    }
+
+    public function hasType($name)
+    {
+        return isset($this->typeServiceIds[$name]);
+    }
+
+    public function getTypeExtensions($name)
+    {
+        $extensions = array();
+
+        if (isset($this->typeExtensionServiceIds[$name])) {
+            foreach ($this->typeExtensionServiceIds[$name] as $serviceId) {
+                $extensions[] = $this->container->get($serviceId);
+            }
+        }
+
+        return $extensions;
+    }
+
+    public function hasTypeExtensions($name)
+    {
+        return isset($this->typeExtensionServiceIds[$name]);
+    }
+}
