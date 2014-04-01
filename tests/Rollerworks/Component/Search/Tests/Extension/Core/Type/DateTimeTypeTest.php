e950d0751b8144700d9f6d47f4fff7590c4b6019
Sebastiaan Stok <s.stok@rollerscapes.net>
2014-04-01 16:09:13 +0200

    fix: Increase testing coverage of core extensions

---
+++ tests/Rollerworks/Component/Search/Tests/Extension/Core/Type/DateTimeTypeTest.php
@@ -0,0 +1,90 @@
+<?php
+
+/*
+ * This file is part of the Rollerworks Search Component package.
+ *
+ * (c) Sebastiaan Stok <s.stok@rollerscapes.net>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Rollerworks\Component\Search\Tests\Extension\Core\Type;
+
+use Rollerworks\Component\Search\Extension\Core\Type\DateTimeType;
+use Rollerworks\Component\Search\Test\FieldTypeTestCase;
+use Symfony\Component\Intl\Util\IntlTestHelper;
+
+class DateTimeTypeTest extends FieldTypeTestCase
+{
+    public function testCreate()
+    {
+        $this->factory->createField('datetime', 'datetime');
+    }
+
+    public function testDifferentTimezonesDateTime()
+    {
+        $field = $this->factory->createField('datetime', 'datetime', array(
+            'model_timezone' => 'America/New_York',
+            'view_timezone' => 'Pacific/Tahiti',
+        ));
+
+        $outputTime = new \DateTime('2010-06-02 03:04:00 Pacific/Tahiti');
+        $outputTime->setTimezone(new \DateTimeZone('America/New_York'));
+
+        $this->assertTransformedEquals($field, $outputTime, '2010-06-02T03:04:00-10:00');
+    }
+
+    public function testWithSeconds()
+    {
+        $field = $this->factory->createField('datetime', 'datetime', array(
+            'model_timezone' => 'UTC',
+            'view_timezone' => 'UTC',
+            'with_seconds' => true,
+        ));
+
+        $outputTime = new \DateTime('2010-06-02 03:04:05 UTC');
+        $this->assertTransformedEquals($field, $outputTime, '2010-06-02T03:04:05Z');
+    }
+
+    public function testDifferentPattern()
+    {
+        $field = $this->factory->createField('datetime', 'datetime', array(
+            'format' => "MM*yyyy*dd",
+        ));
+
+        $outputTime = new \DateTime('2010-06-02');
+        $this->assertTransformedEquals($field, $outputTime, '06*2010*02');
+    }
+
+    public function testHtml5Pattern()
+    {
+        $field = $this->factory->createField('datetime', 'datetime', array(
+            'format' => DateTimeType::HTML5_FORMAT,
+        ));
+
+        $outputTime = new \DateTime('2010-06-02T03:04:05-10:00');
+        $this->assertTransformedEquals($field, $outputTime, "2010-06-02T03:04:05-10:00");
+    }
+
+    public function testWrongInputFails()
+    {
+        $field = $this->factory->createField('datetime', 'datetime', array(
+            'format' => "MM-yyyy-dd",
+        ));
+
+        $this->assertTransformedFails($field, '06*2010*02');
+    }
+
+    protected function setUp()
+    {
+        IntlTestHelper::requireIntl($this);
+
+        parent::setUp();
+    }
+
+    protected function getTestedType()
+    {
+        return 'datetime';
+    }
+}
