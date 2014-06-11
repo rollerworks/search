38f47061f34db38f3174f63b3c967f8543edc942
Sebastiaan Stok <s.stok@rollerscapes.net>
2014-06-11 16:35:17 +0200

    style: update headers, and add .gush.yml

--- tests/Rollerworks/Bundle/SearchBundle/Tests/Resources/Fixtures/config/config.php
+++ tests/Rollerworks/Bundle/SearchBundle/Tests/Resources/Fixtures/config/config.php
@@ -1,7 +1,14 @@
 <?php
 
-/* @var $container \Symfony\Component\DependencyInjection\ContainerBuilder */
-$container->loadFromExtension('rollerworks_search', array(
+/**
+ * This file is part of the RollerworksSearchBundle package.
+ *
+ * (c) 2014 Sebastiaan Stok <s.stok@rollerscapes.net>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
     'metadata' => array(
         'cache_driver' => 'memory',
         'cache_dir' => 'none',
