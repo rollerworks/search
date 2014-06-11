23eb8f6ca5071e50402859d6a92cc660292656bc
Sebastiaan Stok <s.stok@rollerscapes.net>
2014-06-11 15:32:47 +0200

    style: update headers, and add .gush.yml

--- spec/Rollerworks/Component/Search/Fixtures/Entity/Group.php
+++ spec/Rollerworks/Component/Search/Fixtures/Entity/Group.php
@@ -1,5 +1,14 @@
 <?php
 
+/**
+ * This file is part of RollerworksSearch Component package.
+ *
+ * (c) 2012-2014 Sebastiaan Stok <s.stok@rollerscapes.net>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
 namespace Rollerworks\Component\Search\Fixtures\Entity;
 
 use Rollerworks\Component\Search\Metadata as Search;
