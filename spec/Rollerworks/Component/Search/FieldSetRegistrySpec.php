1f83d078814161d558843f199b832c5b67f9858e
Sebastiaan Stok <s.stok@rollerscapes.net>
2015-01-27 16:00:49 +0100

    add FieldSetRegistry for tracking FieldSets

---
+++ spec/Rollerworks/Component/Search/FieldSetRegistrySpec.php
@@ -0,0 +1,104 @@
+<?php
+
+/*
+ * This file is part of the RollerworksSearch Component package.
+ *
+ * (c) Sebastiaan Stok <s.stok@rollerscapes.net>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace spec\Rollerworks\Component\Search;
+
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+use Rollerworks\Component\Search\Exception\InvalidArgumentException;
+use Rollerworks\Component\Search\FieldSet;
+
+class FieldSetRegistrySpec extends ObjectBehavior
+{
+    function it_is_initializable()
+    {
+        $this->shouldHaveType('Rollerworks\Component\Search\FieldSetRegistry');
+    }
+
+    function it_is_a_FieldSetRegistry()
+    {
+        $this->shouldbeAnInstanceOf('Rollerworks\Component\Search\FieldSetRegistryInterface');
+    }
+
+    function it_has_no_FieldSets_when_initialized()
+    {
+        $this->all()->shouldReturn(array());
+    }
+
+    function it_supports_adding_FieldSets()
+    {
+        $fieldSet = new FieldSet('test');
+        $fieldSet->lockConfig();
+
+        $fieldSet2 = new FieldSet('test2');
+        $fieldSet2->lockConfig();
+
+        $this->add($fieldSet);
+        $this->add($fieldSet2);
+
+        $this->all()->shouldReturn(array($fieldSet->getSetName() => $fieldSet, $fieldSet2->getSetName() => $fieldSet2));
+    }
+
+    function it_can_tell_if_a_FieldSet_is_registered()
+    {
+        $fieldSet = new FieldSet('test');
+        $fieldSet->lockConfig();
+
+        $this->add($fieldSet);
+
+        $this->has('test')->shouldReturn(true);
+        $this->has('test2')->shouldReturn(false);
+    }
+
+    function it_supports_getting_a_FieldSet()
+    {
+        $fieldSet = new FieldSet('test');
+        $fieldSet->lockConfig();
+
+        $this->add($fieldSet);
+
+        $this->get('test')->shouldReturn($fieldSet);
+    }
+
+    function it_throws_when_getting_an_unregistered_FieldSet()
+    {
+        $fieldSet = new FieldSet('test');
+        $fieldSet->lockConfig();
+
+        $this->add($fieldSet);
+
+        $this->shouldThrow(
+            new InvalidArgumentException('Unable to get none registered FieldSet "test2".')
+        )->during('get', array('test2'));
+    }
+
+    function it_disallows_overwriting_a_FieldSet()
+    {
+        $fieldSet = new FieldSet('test');
+        $fieldSet->lockConfig();
+
+        $this->add($fieldSet);
+
+        $this->shouldThrow(
+            new InvalidArgumentException('Unable to overwrite already registered FieldSet "test".')
+        )->during('add', array($fieldSet));
+    }
+
+    // open meaning that the configuration is not locked
+    function it_disallows_registering_an_open_FieldSet()
+    {
+        $fieldSet = new FieldSet('test');
+
+        $this->shouldThrow(
+            new InvalidArgumentException('Unable to register none configuration-locked FieldSet "test".')
+        )->during('add', array($fieldSet));
+    }
+}
---
+++ src/FieldSetRegistry.php
@@ -0,0 +1,67 @@
+<?php
+
+/*
+ * This file is part of the RollerworksSearch Component package.
+ *
+ * (c) Sebastiaan Stok <s.stok@rollerscapes.net>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Rollerworks\Component\Search;
+
+use Rollerworks\Component\Search\Exception\InvalidArgumentException;
+
+final class FieldSetRegistry implements FieldSetRegistryInterface
+{
+    private $fieldSets = array();
+
+    /**
+     * {@inheritdoc}
+     */
+    public function has($name)
+    {
+        return isset($this->fieldSets[$name]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function get($name)
+    {
+        if (isset($this->fieldSets[$name])) {
+            return $this->fieldSets[$name];
+        }
+
+        throw new InvalidArgumentException(sprintf('Unable to get none registered FieldSet "%s".', $name));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function add(FieldSet $fieldSet)
+    {
+        $name = $fieldSet->getSetName();
+
+        if (isset($this->fieldSets[$name])) {
+            throw new InvalidArgumentException(sprintf('Unable to overwrite already registered FieldSet "%s".', $name));
+        }
+
+        if (!$fieldSet->isConfigLocked()) {
+            throw new InvalidArgumentException(sprintf('Unable to register none configuration-locked FieldSet "%s".', $name));
+        }
+
+        $this->fieldSets[$name] = $fieldSet;
+
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function all()
+    {
+        return $this->fieldSets;
+    }
+}
