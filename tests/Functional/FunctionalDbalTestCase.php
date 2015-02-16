b693a2d4e66cec6a1846043ae4305b3702e52b00
Sebastiaan Stok <s.stok@rollerscapes.net>
2015-02-16 16:57:17 +0100

    add WhereBuilderTest functional test

---
+++ tests/Functional/FunctionalDbalTestCase.php
@@ -0,0 +1,128 @@
+<?php
+
+/**
+ * PhpStorm.
+ */
+
+namespace Rollerworks\Component\Search\Tests\Doctrine\Dbal\Functional;
+
+use Doctrine\Tests\TestUtil;
+use Rollerworks\Component\Search\Tests\Doctrine\Dbal\DbalTestCase;
+
+abstract class FunctionalDbalTestCase extends DbalTestCase
+{
+    /**
+     * Shared connection when a TestCase is run alone (outside of it's functional suite)
+     *
+     * @var \Doctrine\DBAL\Connection
+     */
+    private static $sharedConn;
+
+    /**
+     * @var \Doctrine\DBAL\Connection
+     */
+    protected $conn;
+
+    /**
+     * @var \Doctrine\DBAL\Logging\DebugStack
+     */
+    protected $sqlLoggerStack;
+
+    protected function resetSharedConn()
+    {
+        if (self::$sharedConn) {
+            self::$sharedConn->close();
+            self::$sharedConn = null;
+        }
+    }
+
+    protected function setUp()
+    {
+        parent::setUp();
+
+        if (!isset(self::$sharedConn)) {
+            self::$sharedConn = TestUtil::getConnection();
+
+            $schema = new \Doctrine\DBAL\Schema\Schema();
+
+            $invoiceTable = $schema->createTable('invoice');
+            $invoiceTable->addColumn('id', 'integer');
+            $invoiceTable->addColumn('status', 'integer');
+            $invoiceTable->addColumn('label', 'string');
+            $invoiceTable->addColumn('customer', 'integer');
+            $invoiceTable->setPrimaryKey(array('id'));
+
+            $customerTable = $schema->createTable('customer');
+            $customerTable->addColumn('id', 'integer');
+            $customerTable->addColumn('name', 'string');
+            $customerTable->addColumn('birthday', 'date');
+            $customerTable->setPrimaryKey(array('id'));
+
+            $platform = self::$sharedConn->getDatabasePlatform();
+            $queries = $schema->toSql($platform);
+
+            foreach ($queries as $query) {
+                self::$sharedConn->exec($query);
+            }
+        }
+
+        $this->conn = self::$sharedConn;
+        $this->sqlLoggerStack = new \Doctrine\DBAL\Logging\DebugStack();
+        $this->conn->getConfiguration()->setSQLLogger($this->sqlLoggerStack);
+    }
+
+    protected function onNotSuccessfulTest(\Exception $e)
+    {
+        if ($e instanceof \PHPUnit_Framework_AssertionFailedError) {
+            throw $e;
+        }
+
+        if(isset($this->sqlLoggerStack->queries) && count($this->sqlLoggerStack->queries)) {
+            $queries = "";
+            $i = count($this->sqlLoggerStack->queries);
+
+            foreach (array_reverse($this->sqlLoggerStack->queries) as $query) {
+                $params = array_map(
+                    function ($p) {
+                        if (is_object($p)) {
+                            return get_class($p);
+                        } else {
+                            return "'".$p."'";
+                        }
+                    },
+                    $query['params'] ?: array()
+                );
+
+                $queries .= ($i+1).". SQL: '".$query['sql']."' Params: ".implode(", ", $params).PHP_EOL;
+                $i--;
+            }
+
+            $trace = $e->getTrace();
+            $traceMsg = '';
+
+            foreach($trace as $part) {
+                if(isset($part['file'])) {
+                    if(strpos($part['file'], "PHPUnit/") !== false) {
+                        // Beginning with PHPUnit files we don't print the trace anymore.
+                        break;
+                    }
+
+                    $traceMsg .= $part['file'].":".$part['line'].PHP_EOL;
+                }
+            }
+
+            $message =
+                "[".get_class($e)."] ".
+                $e->getMessage().
+                PHP_EOL.PHP_EOL.
+                "With queries:".PHP_EOL.
+                $queries.PHP_EOL.
+                "Trace:".PHP_EOL.
+                $traceMsg;
+
+            throw new \Exception($message, (int) $e->getCode(), $e);
+        }
+
+        throw $e;
+    }
+}
---
+++ tests/Functional/WhereBuilderTest.php
@@ -0,0 +1,432 @@
+<?php
+
+/*
+ * This file is part of the RollerworksSearch Component package.
+ *
+ * (c) Sebastiaan Stok <s.stok@rollerscapes.net>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Rollerworks\Component\Search\Tests\Doctrine\Dbal\Functional;
+
+use Doctrine\DBAL\Connection;
+use Rollerworks\Component\Search\SearchCondition;
+use Rollerworks\Component\Search\SearchConditionBuilder;
+use Rollerworks\Component\Search\Tests\Doctrine\Dbal\Stub\InvoiceNumber;
+use Rollerworks\Component\Search\Value\Compare;
+use Rollerworks\Component\Search\Value\PatternMatch;
+use Rollerworks\Component\Search\Value\Range;
+use Rollerworks\Component\Search\Value\SingleValue;
+use Rollerworks\Component\Search\ValuesGroup;
+
+/**
+ * Functional WhereBuilderTest, ensures queries are executable.
+ * This tests does not ensure the correct result is returned,
+ * this handled by another test-class.
+ *
+ * @group functional
+ */
+final class WhereBuilderTest extends FunctionalDbalTestCase
+{
+    private function getWhereBuilder(SearchCondition $condition, Connection $connection = null)
+    {
+        $whereBuilder = $this->getDbalFactory()->createWhereBuilder(
+            $connection ?: $this->conn,
+            $condition
+        );
+
+        $whereBuilder->setField('customer', 'customer', 'integer', 'i');
+        $whereBuilder->setField('customer_name', 'name', 'string', 'c');
+        $whereBuilder->setField('customer_birthday', 'birthday', 'string', 'c'); // don't use date as this breaks the binding
+        $whereBuilder->setField('status', 'status', 'integer', 'i');
+        $whereBuilder->setField('label', 'label', 'string', 'i');
+
+        return $whereBuilder;
+    }
+
+    private function assertQueryIsExecutable($conditionOrWhere, $valuesEmbedded = false)
+    {
+        if ($conditionOrWhere instanceof SearchCondition) {
+            $whereBuilder = $this->getWhereBuilder($conditionOrWhere);
+        } else {
+            $whereBuilder = $conditionOrWhere;
+        }
+
+        $whereClause = $whereBuilder->getWhereClause($valuesEmbedded);
+
+        $query = "SELECT i.*, c.* FROM invoice AS i JOIN customer AS c ON (c.id = i.customer) WHERE ".$whereClause;
+
+        if (!$valuesEmbedded) {
+            $prepare = $this->conn->prepare($query);
+            $whereBuilder->bindParameters($prepare);
+
+            $this->assertNotNull($prepare->execute());
+        } else {
+            $this->assertNotNull($this->conn->query($query));
+        }
+    }
+
+    public function testSimpleQuery()
+    {
+        $condition = SearchConditionBuilder::create($this->getFieldSet())
+            ->field('customer')
+                ->addSingleValue(new SingleValue(2))
+                ->addSingleValue(new SingleValue(5))
+            ->end()
+        ->getSearchCondition();
+
+        $this->assertQueryIsExecutable($condition);
+    }
+
+
+    public function testQueryWithEmbeddedValues()
+    {
+        $condition = SearchConditionBuilder::create($this->getFieldSet())
+            ->field('customer')
+                ->addSingleValue(new SingleValue(2))
+                ->addSingleValue(new SingleValue(5))
+            ->end()
+        ->getSearchCondition();
+
+        $this->assertQueryIsExecutable($condition, true);
+    }
+
+    public function testExcludes()
+    {
+        $condition = SearchConditionBuilder::create($this->getFieldSet())
+            ->field('customer')
+                ->addExcludedValue(new SingleValue(2))
+                ->addExcludedValue(new SingleValue(5))
+            ->end()
+        ->getSearchCondition();
+
+        $this->assertQueryIsExecutable($condition);
+    }
+
+    public function testIncludesAndExcludes()
+    {
+        $condition = SearchConditionBuilder::create($this->getFieldSet())
+            ->field('customer')
+                ->addSingleValue(new SingleValue(2))
+                ->addExcludedValue(new SingleValue(5))
+            ->end()
+        ->getSearchCondition();
+
+        $this->assertQueryIsExecutable($condition);
+    }
+
+    public function testRanges()
+    {
+        $condition = SearchConditionBuilder::create($this->getFieldSet())
+            ->field('customer')
+                ->addRange(new Range(2, 5))
+                ->addRange(new Range(10, 20))
+                ->addRange(new Range(60, 70, false))
+                ->addRange(new Range(100, 150, true, false))
+            ->end()
+        ->getSearchCondition();
+
+        $this->assertQueryIsExecutable($condition);
+    }
+
+    public function testExcludedRanges()
+    {
+        $condition = SearchConditionBuilder::create($this->getFieldSet())
+            ->field('customer')
+                ->addExcludedRange(new Range(2, 5))
+                ->addExcludedRange(new Range(10, 20))
+                ->addExcludedRange(new Range(60, 70, false))
+                ->addExcludedRange(new Range(100, 150, true, false))
+            ->end()
+        ->getSearchCondition();
+
+        $this->assertQueryIsExecutable($condition);
+    }
+
+    public function testSingleComparison()
+    {
+        $condition = SearchConditionBuilder::create($this->getFieldSet())
+            ->field('customer')
+                ->addComparison(new Compare(2, '>'))
+            ->end()
+        ->getSearchCondition();
+
+        $this->assertQueryIsExecutable($condition);
+    }
+
+    public function testMultipleComparisons()
+    {
+        $condition = SearchConditionBuilder::create($this->getFieldSet())
+            ->field('customer')
+                ->addComparison(new Compare(2, '>'))
+                ->addComparison(new Compare(10, '<'))
+            ->end()
+        ->getSearchCondition();
+
+        $this->assertQueryIsExecutable($condition);
+    }
+
+    public function testMultipleComparisonsWithGroups()
+    {
+        // Use two subgroups here as the comparisons are AND to each other
+        // but applying them in the head group would ignore subgroups
+        $condition = SearchConditionBuilder::create($this->getFieldSet())
+            ->group()
+                ->field('customer')
+                    ->addComparison(new Compare(2, '>'))
+                    ->addComparison(new Compare(10, '<'))
+                ->end()
+            ->end()
+            ->group()
+                ->field('customer')
+                    ->addComparison(new Compare(30, '>'))
+                ->end()
+            ->end()
+        ->getSearchCondition();
+
+        $this->assertQueryIsExecutable($condition);
+    }
+
+    public function testExcludingComparisons()
+    {
+        $condition = SearchConditionBuilder::create($this->getFieldSet())
+            ->field('customer')
+                ->addComparison(new Compare(2, '<>'))
+                ->addComparison(new Compare(5, '<>'))
+            ->end()
+        ->getSearchCondition();
+
+        $this->assertQueryIsExecutable($condition);
+    }
+
+    public function testExcludingComparisonsWithNormal()
+    {
+        $condition = SearchConditionBuilder::create($this->getFieldSet())
+            ->field('customer')
+                ->addComparison(new Compare(2, '<>'))
+                ->addComparison(new Compare(5, '<>'))
+                ->addComparison(new Compare(30, '>'))
+            ->end()
+        ->getSearchCondition();
+
+        $this->assertQueryIsExecutable($condition);
+    }
+
+    public function testPatternMatchers()
+    {
+        $condition = SearchConditionBuilder::create($this->getFieldSet())
+            ->field('customer_name')
+                ->addPatternMatch(new PatternMatch('foo', PatternMatch::PATTERN_STARTS_WITH))
+                ->addPatternMatch(new PatternMatch('fo\\\'o', PatternMatch::PATTERN_STARTS_WITH))
+                ->addPatternMatch(new PatternMatch('bar', PatternMatch::PATTERN_NOT_ENDS_WITH, true))
+                ->addPatternMatch(new PatternMatch('(foo|bar)', PatternMatch::PATTERN_REGEX))
+                ->addPatternMatch(new PatternMatch('(doctor|who)', PatternMatch::PATTERN_REGEX, true))
+            ->end()
+        ->getSearchCondition();
+
+        $this->assertQueryIsExecutable($condition);
+    }
+
+    public function testSubGroups()
+    {
+        $condition = SearchConditionBuilder::create($this->getFieldSet())
+            ->group()
+                ->field('customer')->addSingleValue(new SingleValue(2))->end()
+            ->end()
+            ->group()
+                ->field('customer')->addSingleValue(new SingleValue(3))->end()
+            ->end()
+        ->getSearchCondition();
+
+        $this->assertQueryIsExecutable($condition);
+    }
+
+    public function testSubGroupWithRootCondition()
+    {
+        $condition = SearchConditionBuilder::create($this->getFieldSet())
+            ->field('customer')
+                ->addSingleValue(new SingleValue(2))
+            ->end()
+            ->group()
+                ->field('customer_name')
+                    ->addPatternMatch(new PatternMatch('foo', PatternMatch::PATTERN_STARTS_WITH))
+                ->end()
+            ->end()
+        ->getSearchCondition();
+
+        $this->assertQueryIsExecutable($condition);
+    }
+
+    public function testOrGroupRoot()
+    {
+        $condition = SearchConditionBuilder::create($this->getFieldSet(), ValuesGroup::GROUP_LOGICAL_OR)
+            ->field('customer')
+                ->addSingleValue(new SingleValue(2))
+            ->end()
+            ->field('customer_name')
+                ->addPatternMatch(new PatternMatch('foo', PatternMatch::PATTERN_STARTS_WITH))
+            ->end()
+        ->getSearchCondition();
+
+        $this->assertQueryIsExecutable($condition);
+    }
+
+    public function testSubOrGroup()
+    {
+        $condition = SearchConditionBuilder::create($this->getFieldSet())
+            ->group()
+                ->group(ValuesGroup::GROUP_LOGICAL_OR)
+                    ->field('customer')
+                        ->addSingleValue(new SingleValue(2))
+                    ->end()
+                    ->field('customer_name')
+                        ->addPatternMatch(new PatternMatch('foo', PatternMatch::PATTERN_STARTS_WITH))
+                    ->end()
+                ->end()
+            ->end()
+        ->getSearchCondition();
+
+        $this->assertQueryIsExecutable($condition);
+    }
+
+    public function testValueConversion()
+    {
+        $condition = SearchConditionBuilder::create($this->getFieldSet())
+            ->field('label')
+                ->addSingleValue(new SingleValue(InvoiceNumber::createFromString('2015-001')))
+                ->addSingleValue(new SingleValue(InvoiceNumber::createFromString('2015-005')))
+            ->end()
+        ->getSearchCondition();
+
+        $this->assertQueryIsExecutable($condition);
+    }
+
+    /**
+     * @dataProvider provideFieldConversionTests
+     *
+     * @param boolean $valuesEmbedding
+     */
+    public function testFieldConversion($valuesEmbedding = false)
+    {
+        $condition = SearchConditionBuilder::create($this->getFieldSet())
+            ->field('customer')
+                ->addSingleValue(new SingleValue(2))
+            ->end()
+        ->getSearchCondition();
+
+        $whereBuilder = $this->getWhereBuilder($condition);
+
+        $converter = $this->getMock('Rollerworks\Component\Search\Doctrine\Dbal\SqlFieldConversionInterface');
+        $converter
+            ->expects($this->atLeastOnce())
+            ->method('convertSqlField')
+            ->will($this->returnCallback(function ($column) {
+                return "CAST($column AS customer_type)";
+            }))
+        ;
+
+        $whereBuilder->setConverter('customer', $converter);
+        $this->assertQueryIsExecutable($whereBuilder, $valuesEmbedding);
+    }
+
+    /**
+     * @dataProvider provideSqlValueConversionTests
+     *
+     * @param boolean $valuesEmbedding
+     * @param boolean $valueReqEmbedding
+     */
+    public function testSqlValueConversion($valuesEmbedding = false, $valueReqEmbedding = false)
+    {
+        $fieldSet = $this->getFieldSet();
+        $condition = SearchConditionBuilder::create($fieldSet)
+            ->field('customer')
+                ->addSingleValue(new SingleValue(2))
+            ->end()
+        ->getSearchCondition();
+
+        $whereBuilder = $this->getWhereBuilder($condition);
+
+        $converter = $this->getMock('Rollerworks\Component\Search\Doctrine\Dbal\SqlValueConversionInterface');
+        $converter
+            ->expects($this->atLeastOnce())
+            ->method('convertSqlValue')
+            ->will($this->returnCallback(function ($input) {
+                return "CAST($input AS integer)";
+            }))
+        ;
+
+        $converter
+            ->expects(!$valuesEmbedding ? $this->atLeastOnce() : $this->any())
+            ->method('valueRequiresEmbedding')
+            ->will($this->returnValue($valueReqEmbedding))
+        ;
+
+        $converter
+            ->expects($this->atLeastOnce())
+            ->method('requiresBaseConversion')
+            ->will($this->returnValue(false))
+        ;
+
+        $converter
+            ->expects($this->atLeastOnce())
+            ->method('convertValue')
+            ->will($this->returnArgument(0))
+        ;
+
+        $whereBuilder->setConverter('customer', $converter);
+        $this->assertQueryIsExecutable($whereBuilder, $valuesEmbedding);
+    }
+
+    /**
+     * @dataProvider provideConversionStrategyTests
+     *
+     * @param boolean $valuesEmbedding
+     */
+    public function testConversionStrategy($valuesEmbedding = false)
+    {
+        $date = new \DateTime('2001-01-15', new \DateTimeZone('UTC'));
+
+        $fieldSet = $this->getFieldSet(false);
+        $fieldSet->add('customer_birthday', 'birthday');
+
+        $fieldSet = $fieldSet->getFieldSet();
+
+        $condition = SearchConditionBuilder::create($fieldSet)
+            ->field('customer_birthday')
+                ->addSingleValue(new SingleValue(18))
+                ->addSingleValue(new SingleValue($date, '2001-01-15'))
+            ->end()
+        ->getSearchCondition();
+
+        $whereBuilder = $this->getWhereBuilder($condition);
+        $this->assertQueryIsExecutable($whereBuilder, $valuesEmbedding);
+    }
+
+    public static function provideFieldConversionTests()
+    {
+        return array(
+            array(false),
+            array(true),
+        );
+    }
+
+    public static function provideSqlValueConversionTests()
+    {
+        return array(
+            array(false),
+            array(false, true),
+            array(true),
+            array(true, true),
+        );
+    }
+
+    public static function provideConversionStrategyTests()
+    {
+        return array(
+            array(false),
+            array(true),
+        );
+    }
+}
