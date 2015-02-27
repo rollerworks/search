0e075fa8f91a11c59a32fa8de4f2bb4be19cd4d1
Sebastiaan Stok <s.stok@rollerscapes.net>
2015-02-27 15:04:20 +0100

    [doc] fix wrong html_short_title
    [skip ci]

--- doc/conf.py
+++ doc/conf.py
@@ -126,7 +126,7 @@ html_theme_path = ['_themes']
 #html_title = None
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
-html_short_title = 'RollerworksSearch'
+html_short_title = 'RollerworksSearch Doctrine DBAL'
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
