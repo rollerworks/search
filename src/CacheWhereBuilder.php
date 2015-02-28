0663f7d1e26bedab9f2046492e45ec044bae4b93
Sebastiaan Stok <s.stok@rollerscapes.net>
2015-02-28 15:55:26 +0100

    merge AbstractCache.. to CacheWhereBuilder
    The AbstractCacheWhereBuilder is currently used by the ORM
    package but it should not rely on this class, and was actually done
    for historical reasons
    
    The setCacheKeySuffix() method is now deprecated as it's supper seeded
    by the callback support

--- src/CacheWhereBuilder.php
+++ src/CacheWhereBuilder.php
@@ -12,13 +12,13 @@
 namespace Rollerworks\Component\Search\Doctrine\Dbal;
 
 use Doctrine\Common\Cache\Cache;
-use Rollerworks\Component\Search\Exception\UnexpectedTypeException;
+use Rollerworks\Component\Search\Exception\BadMethodCallException;
 
 /***
  * Handles caching of the Doctrine DBAL WhereBuilder.
  *
  * Note. For best performance caching of the WhereClause should be done on a
- * per user-session fieldset basis. This ensures enough uniqueness and
+ * per user-session FieldSet basis. This ensures enough uniqueness and
  * no complex serialization.
  *
  * This checks if there is a cached result, if not it delegates
@@ -27,21 +27,50 @@ use Rollerworks\Component\Search\Exception\UnexpectedTypeException;
  * Instead of calling getWhereClause() on the WhereBuilder class
  * you should call getWhereClause() on this class instead.
  *
- * WARNING. Any changes to the mapping-data should invalidate the cache
- * the system does not do this automatically.
+ * WARNING. Any changes to the mapping-data should invalidate the cache,
+ * the system doesn't do this automatically.
  *
  * @author Sebastiaan Stok <s.stok@rollerscapes.net>
  */
-class CacheWhereBuilder extends AbstractCacheWhereBuilder implements WhereBuilderInterface
+class CacheWhereBuilder implements WhereBuilderInterface
 {
+    /**
+     * @var Cache
+     */
+    private $cacheDriver;
+
+    /**
+     * @var int
+     */
+    private $cacheLifeTime;
+
+    /**
+     * @var WhereBuilderInterface
+     */
+    private $whereBuilder;
+
+    /**
+     * @var string
+     */
+    private $cacheKey;
+
+    /**
+     * @var string
+     */
+    private $keySuffix = '';
+
+    /**
+     * @var string
+     */
+    private $whereClause;
+
     /**
      * Constructor.
      *
      * @param WhereBuilderInterface $whereBuilder The WhereBuilder to use for generating and updating the query
      * @param Cache                 $cacheDriver  Doctrine Cache instance
      * @param int                   $lifeTime     Lifetime in seconds after which the cache is expired
-     *
-     * @throws UnexpectedTypeException when the whereBuilder is invalid
+     *                                            Set this 0 to never expire.
      */
     public function __construct(WhereBuilderInterface $whereBuilder, Cache $cacheDriver, $lifeTime = 0)
     {
@@ -51,10 +80,58 @@ class CacheWhereBuilder extends AbstractCacheWhereBuilder implements WhereBuilde
     }
 
     /**
+     * Set the cache key.
+     *
+     * This method also accepts a callback that can calculate the key for you.
+     * The callback will receive wherebuilder.
+     *
+     * @param string   $key
+     * @param callable $callback
+     *
+     * @return self
+     *
+     * @throws BadMethodCallException
+     */
+    public function setCacheKey($key = null, $callback = null)
+    {
+        if ((null === $key && null === $callback) || ($callback && !is_callable($callback))) {
+            throw new BadMethodCallException('Either a key or legal callback must be given.');
+        }
+
+        if ($callback) {
+            $key = call_user_func($callback, $this->whereBuilder);
+        }
+
+        $this->cacheKey = (string) $key;
+
+        return $this;
+    }
+
+    /**
+     * Set an extra suffix for the caching key.
+     *
+     * This allows to make the key more unique.
+     * For example, you can set the key to calculate automatically,
+     * and add this suffix to ensure there is no problem with different mapping.
+     *
+     * @param string $key
+     *
+     * @return self
+     *
+     * @deprecated since version 1.0.0-beta7, to be removed in 2.0.
+     *             Use setCacheKey() instead with a unique key.
+     */
+    public function setCacheKeySuffix($key)
+    {
+        $this->keySuffix = $key.'_';
+
+        return $this;
+    }
+
+    /**
      * Returns the generated/cached where-clause.
      *
      * @see WhereBuilder::getWhereClause()
-     *
      *
      * @return string
      */
@@ -64,12 +141,10 @@ class CacheWhereBuilder extends AbstractCacheWhereBuilder implements WhereBuilde
             return $this->whereClause;
         }
 
-        $cacheKey = 'rw_search.doctrine.dbal.where.'.$this->cacheKey.($this->keySuffix ? '_'.$this->keySuffix : '');
+        $cacheKey = 'rw_search.doctrine.dbal.where.'.$this->cacheKey;
 
         if ($this->cacheDriver->contains($cacheKey)) {
-            $data = $this->cacheDriver->fetch($cacheKey);
-
-            $this->whereClause = $data;
+            $this->whereClause = $this->cacheDriver->fetch($cacheKey);
         } else {
             $this->whereClause = $this->whereBuilder->getWhereClause();
 
@@ -81,5 +156,24 @@ class CacheWhereBuilder extends AbstractCacheWhereBuilder implements WhereBuilde
         }
 
         return $this->whereClause;
+    }
+
+    /**
+     * Returns the original WhereBuilder that is used for generating
+     * the where-clause.
+     *
+     * @return WhereBuilderInterface
+     */
+    public function getInnerWhereBuilder()
+    {
+        return $this->whereBuilder;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getSearchCondition()
+    {
+        return $this->whereBuilder->getSearchCondition();
     }
 }
