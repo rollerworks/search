0bad2f3d336ac5e9080ea24a5ca2de70193286e0
Sebastiaan Stok <s.stok@rollerscapes.net>
2015-03-12 10:44:27 +0100

    fix compatibility for Symfony 2.7

--- src/Type/FieldTypeValidatorExtension.php
+++ src/Type/FieldTypeValidatorExtension.php
@@ -14,6 +14,7 @@ namespace Rollerworks\Component\Search\Extension\Symfony\Validator\Type;
 use Rollerworks\Component\Search\AbstractFieldTypeExtension;
 use Rollerworks\Component\Search\FieldConfigInterface;
 use Symfony\Component\OptionsResolver\Options;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 
 /**
@@ -39,7 +40,7 @@ class FieldTypeValidatorExtension extends AbstractFieldTypeExtension
     /**
      * {@inheritdoc}
      */
-    public function setDefaultOptions(OptionsResolverInterface $optionsResolver)
+    public function configureOptions(OptionsResolver $optionsResolver)
     {
         $optionsResolver->setDefaults(
             array(
@@ -48,19 +49,31 @@ class FieldTypeValidatorExtension extends AbstractFieldTypeExtension
             )
         );
 
-        $optionsResolver->setAllowedTypes(
-            array(
-                'constraints' => array('array', 'Symfony\Component\Validator\Constraint'),
-                'validation_groups' => array('array'),
-            )
-        );
+        // BC layer for Symfony 2.7 and 3.0
+        if ($optionsResolver instanceof OptionsResolverInterface) {
+            $optionsResolver->setAllowedTypes(
+                array(
+                    'constraints' => array('array', 'Symfony\Component\Validator\Constraint'),
+                    'validation_groups' => array('array'),
+                )
+            );
 
-        $optionsResolver->setNormalizers(
-            array(
-                'constraints' => function (Options $options, $value) {
+            $optionsResolver->setNormalizers(
+                array(
+                    'constraints' => function (Options $options, $value) {
+                        return !is_array($value) ? array($value) : $value;
+                    },
+                )
+            );
+        } else {
+            $optionsResolver->setAllowedTypes('constraints', array('array', 'Symfony\Component\Validator\Constraint'));
+            $optionsResolver->setAllowedTypes('validation_groups', array('array'));
+            $optionsResolver->setNormalizer(
+                'constraints',
+                function (Options $options, $value) {
                     return !is_array($value) ? array($value) : $value;
-                },
-            )
-        );
+                }
+            );
+        }
     }
 }
