e38eb89a46e92e54a4b490cbe1b48863f73d13b4
Sebastiaan Stok <s.stok@rollerscapes.net>
2015-07-06 13:58:38 +0200

    fix functional tests

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -23,3 +23,23 @@ if (!file_exists(__DIR__.'/../vendor/autoload.php')) {
 $loader = require __DIR__.'/../vendor/autoload.php';
 
 AnnotationRegistry::registerLoader([$loader, 'loadClass']);
+
+// Don't use the system tempdir on Windows as that fails to work!
+// The path gets to long when it also includes the 6 character hash of the Container with functional tests.
+call_user_func(
+    function () {
+        if (defined('PHP_WINDOWS_VERSION_MAJOR')) {
+            $rootDir = substr(realpath(substr(__DIR__, 0, strpos(__DIR__, '\\') + 1)), 0, -1);
+        } else {
+            $rootDir = sys_get_temp_dir();
+        }
+
+        $rootDir .= '/.tmp_c';
+
+        putenv('TMPDIR='.$rootDir);
+
+        if (!is_dir($rootDir)) {
+            mkdir($rootDir, 0777, true);
+        }
+    }
+);
