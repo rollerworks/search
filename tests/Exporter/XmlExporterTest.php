277b3383189693b5d2780f2cab9420f25a6a0c68
Sebastiaan Stok <s.stok@rollerscapes.net>
2015-08-11 11:06:11 +0200

    fix exporter component supporting (NOT_)EQUALS

--- tests/Exporter/XmlExporterTest.php
+++ tests/Exporter/XmlExporterTest.php
@@ -178,6 +178,10 @@ final class XmlExporterTest extends SearchConditionExporterTestCase
                         <pattern-matcher type="regex" case-insensitive="false">^foo|bar?</pattern-matcher>
                         <pattern-matcher type="not_contains" case-insensitive="false">value4</pattern-matcher>
                         <pattern-matcher type="not_contains" case-insensitive="true">value5</pattern-matcher>
+                        <pattern-matcher type="equals" case-insensitive="false">value9</pattern-matcher>
+                        <pattern-matcher type="not_equals" case-insensitive="false">value10</pattern-matcher>
+                        <pattern-matcher type="equals" case-insensitive="true">value11</pattern-matcher>
+                        <pattern-matcher type="not_equals" case-insensitive="true">value12</pattern-matcher>
                     </pattern-matchers>
                 </field>
               </fields>
