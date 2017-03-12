21a7f8cd4e7cdcf1917531da6880f49f843c8f56
Sebastiaan Stok <s.stok@rollerscapes.net>
2017-03-12 11:40:44 +0100

    Add LazyExtension for loading types lazily

---
+++ src/Extension/LazyExtension.php
@@ -0,0 +1,112 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the RollerworksSearch package.
+ *
+ * (c) Sebastiaan Stok <s.stok@rollerscapes.net>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Rollerworks\Component\Search\Extension;
+
+use Psr\Container\ContainerInterface;
+use Rollerworks\Component\Search\Exception\InvalidArgumentException;
+use Rollerworks\Component\Search\Field\FieldType;
+use Rollerworks\Component\Search\Loader\ClosureContainer;
+use Rollerworks\Component\Search\SearchExtension;
+
+/**
+ * Provides a way to lazy load types from the Container.
+ *
+ * @author Sebastiaan Stok <s.stok@rollerscapes.net>
+ */
+final class LazyExtension implements SearchExtension
+{
+    /**
+     * @var ContainerInterface
+     */
+    private $typeContainer;
+
+    /**
+     * @var array[]
+     */
+    private $typeExtensionServices = [];
+
+    /**
+     * Constructor.
+     *
+     * @param ContainerInterface $typeContainer
+     * @param iterable[]         $typeExtensions
+     */
+    public function __construct(ContainerInterface $typeContainer, iterable $typeExtensions)
+    {
+        $this->typeContainer = $typeContainer;
+        $this->typeExtensionServices = $typeExtensions;
+    }
+
+    /**
+     * Creates a new LazyExtension with easy factories for lazy loading.
+     *
+     * @param iterable   $types          FQCN => \Closure factory
+     * @param iterable[] $typeExtensions
+     *
+     * @return LazyExtension
+     */
+    public static function create(iterable $types, iterable $typeExtensions = []): LazyExtension
+    {
+        return new self(new ClosureContainer($types), $typeExtensions);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getType(string $name): FieldType
+    {
+        if (!$this->typeContainer->has($name)) {
+            throw new InvalidArgumentException(
+                sprintf('The field type "%s" is not registered with the service container.', $name)
+            );
+        }
+
+        return $this->typeContainer->get($name);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function hasType(string $name): bool
+    {
+        return $this->typeContainer->has($name);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getTypeExtensions(string $name): array
+    {
+        $extensions = [];
+
+        if (isset($this->typeExtensionServices[$name])) {
+            foreach ($this->typeExtensionServices[$name] as $extensionId => $extension) {
+                $extensions[] = $extension;
+
+                // validate result of getExtendedType() to ensure it is consistent with the service definition
+                if ($extension->getExtendedType() !== $name) {
+                    throw new InvalidArgumentException(
+                        sprintf('The extended type specified for the service "%s" does not match the actual extended type. Expected "%s", given "%s".',
+                            $extensionId,
+                            $name,
+                            $extension->getExtendedType()
+                        )
+                    );
+                }
+            }
+        }
+
+        return $extensions;
+    }
+}
