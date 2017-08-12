855b9500b64b8a822025f689cf927d214154e9d9
Sebastiaan Stok <s.stok@rollerscapes.net>
2017-08-12 17:09:23 +0200

    Fix tests

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -17,10 +17,6 @@ if (!file_exists(__DIR__.'/../vendor/autoload.php')) {
     throw new \RuntimeException('Did not find vendor/autoload.php. Did you run "composer install --prefer-source --dev"?');
 }
 
-if (version_compare(PHP_VERSION, '5.4', '>=') && gc_enabled()) {
-    // Disabling Zend Garbage Collection to prevent segfaults with PHP5.4+
-    // https://bugs.php.net/bug.php?id=53976
-    gc_disable();
-}
+date_default_timezone_set('UTC');
 
 require __DIR__.'/../vendor/autoload.php';
