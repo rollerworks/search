8a3d11b83ecc83f3ed42a6c08681bb034d9dc772
Dalibor Karlović <dalibor.karlovic@flexolabs.io>
2018-12-12 10:40:57 +0100

    [Core] ParameterBag, initial

---
+++ lib/Core/ParameterBag.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the RollerworksSearch package.
+ *
+ * (c) Sebastiaan Stok <s.stok@rollerscapes.net>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Rollerworks\Component\Search;
+
+class ParameterBag
+{
+    private $parameters = [];
+
+    public function __construct(array $parameters = [])
+    {
+        foreach ($parameters as $name => $value) {
+            $this->setParameter($name, $value);
+        }
+    }
+
+    public function setParameter(string $name, $value): void
+    {
+        $this->parameters['{'.$name.'}'] = $value;
+    }
+
+    public function injectParameters($template): string
+    {
+        return str_replace(array_keys($this->parameters), array_values($this->parameters), $template);
+    }
+}
