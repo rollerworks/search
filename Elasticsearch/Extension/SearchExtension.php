0436678427f9a1fbe59589499f04e184f128c4ee
Sebastiaan <s.stok@rollerscapes.net>
2020-04-13 15:00:38 +0200

    Fix PHPStan reported problems

--- Elasticsearch/Extension/SearchExtension.php
+++ Elasticsearch/Extension/SearchExtension.php
@@ -50,8 +50,10 @@ class SearchExtension implements QueryCollectionExtensionInterface
     {
         $request = $this->requestStack->getCurrentRequest();
 
-        /** @var SearchCondition $condition */
-        if (!$request || null === $condition = $request->attributes->get('_api_search_condition')) {
+        /** @var SearchCondition|null $condition */
+        $condition = $request->attributes->get('_api_search_condition');
+
+        if (null === $condition) {
             return;
         }
 
